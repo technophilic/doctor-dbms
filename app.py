0c13bfff65b8f0527eecb5925d57a40527943d58
Vineeth S <svineth.face@gmail.com>
2017-09-09 01:11:33 +0530

    cors

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import json
 app= Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def index():
     x={'a':'loki','b':'lok'}
     return json.dumps(x)
